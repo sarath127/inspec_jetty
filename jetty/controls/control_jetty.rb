a2a3a28533f4df2d813b0f4fc2f9e9364a32f57f
sarath127 <saraths674@gmail.com>
2018-01-09 16:13:08 +0530

    43 test cases

--- jetty/controls/control_jetty.rb
+++ jetty/controls/control_jetty.rb
@@ -111,9 +111,9 @@ control 'jetty-06' do
       it { should be_file }
   end
   describe file(jetty_xml_conf)do
-     #jetty script file should exist and it should be executable
-     it { should exist }
-     it { should be_file}
+      #configuration file for configuring the entire server
+      it { should exist }
+      it { should be_file}
   end
 
 end
@@ -133,9 +133,9 @@ control 'jetty-07'do
   end
   describe passwd do
      #passwd file should include user "jetty"
-     its('users'){ should include 'jetty'}
+      its('users'){ should include 'jetty'}
      #passwd file should not contain forbidden_user
-     its('users'){ should_not include 'forbidden_user' }
+      its('users'){ should_not include 'forbidden_user' }
   end
 end
 
@@ -143,11 +143,11 @@ control 'jetty-08'do
   impact 1.0
   title 'security and performance checking'
   desc  'checking minimum and maximum thread size '
-  describe ini( jetty_conf )do
+  describe ini(jetty_conf)do
     #default minimum thread size is "10" it can be replaced by "8" to make jetty run in optimised way
-    its(['jetty.threadPool.minThreads']) { should eq '8' }
+      its(['jetty.threadPool.minThreads']) { should eq '8' }
     #default maximum thread size is "200" it can be replaced by "100" to make jetty run in optimised way
-    its(['jetty.threadPool.maxThreads']) { should eq '100' }
+      its(['jetty.threadPool.maxThreads']) { should eq '100' }
   end
 end
 
@@ -155,11 +155,11 @@ control 'jetty-09'do
   impact 1.0
   title 'checking security scheme and security port'
   desc  'checking security scheme should be https and security port should be 8443'
-  describe ini( jetty_conf )do
+  describe ini(jetty_conf)do
     #security scheme of jetty server should be "https"
-    its(['jetty.httpConfig.secureScheme']) { should eq 'https' }
+      its(['jetty.httpConfig.secureScheme']) { should eq 'https' }
     #secure port of jetty server should be "8443"
-    its(['jetty.httpConfig.securePort']) { should eq '8443' }
+      its(['jetty.httpConfig.securePort']) { should eq '8443' }
   end
 end
 
@@ -167,11 +167,11 @@ control 'jetty-10'do
   impact 1.0
   title 'cheking host address and port configuration'
   desc  'jetty host address is assgined to 0.0.0.0 and port is 8080'
-  describe ini( jetty_conf )do
-    #host is the network interface this connector binds to as an IP address or a hostname. If null or 0.0.0.0, bind to all interfaces.
-    its(['jetty.http.host']) { should eq '0.0.0.0' }
-    #default port nubmer of jetty server is 8080
-    its(['jetty.http.port']) { should eq '8080' }
+  describe ini(jetty_conf)do
+      #host is the network interface this connector binds to as an IP address or a hostname. If null or 0.0.0.0, bind to all interfaces.
+      its(['jetty.http.host']) { should eq '0.0.0.0' }
+      #default port nubmer of jetty server is 8080
+      its(['jetty.http.port']) { should eq '8080' }
   end
 end
 
@@ -179,9 +179,9 @@ control 'jetty-11'do
   impact 1.0
   title 'cheking moniterdPath to deploy'
   desc  'inorder to deploy the webapplications all necessary file should be stored in mointered path '
-  describe ini( jetty_conf )do
-    #The directory to scan for possible deployable Web Applications (or Deployment Descriptor XML files).
-    its(['jetty.deploy.monitoredPath']) {should eq '/var/www/webapps'}
+  describe ini(jetty_conf)do
+  its(['jetty.deploy.monitoredPath']) {should eq '/var/www/webapps'}
+     #The directory to scan for possible deployable Web Applications (or Deployment Descriptor XML files).
   end
 end
 
@@ -189,13 +189,13 @@ control 'jetty-12'do
   impact 1.0
   title 'cheking soLingerTime,compilance and scan intraval'
   desc  'cheking soLingerTime,compilance and scan intraval'
-  describe ini( jetty_conf )do
-    #A value greater than zero sets the socket SO_LINGER value in milliseconds. Jetty attempts to gently close all TCP/IP connections with proper half close semantics, so a linger timeout should not be required and thus the default is -1.
-    its(['jetty.http.soLingerTime']) { should eq '-1' }
-    #the defualt compilance scheme is RFC7230 it is changed to RFC2616 to make jetty secue
-    its(['jetty.http.compliance']) { should eq 'RFC2616' }
-    #Number of seconds between scans of the provided monitoredDirName. A value of 0 disables the continuous hot deployment scan, Web Applications will be deployed on startup only.
-    its(['jetty.deploy.scanInterval']) { should eq '1' }
+  describe ini(jetty_conf)do
+     #A value greater than zero sets the socket SO_LINGER value in milliseconds. Jetty attempts to gently close all TCP/IP connections with proper half close semantics, so a linger timeout should not be required and thus the default is -1.
+     its(['jetty.http.soLingerTime']) { should eq '-1' }
+     #the defualt compilance scheme is RFC7230 it is changed to RFC2616 to make jetty secue
+     its(['jetty.http.compliance']) { should eq 'RFC2616' }
+     #Number of seconds between scans of the provided monitoredDirName. A value of 0 disables the continuous hot deployment scan, Web Applications will be deployed on startup only.
+     its(['jetty.deploy.scanInterval']) { should eq '1' }
   end
 end
 
@@ -203,11 +203,11 @@ control 'jetty-13'do
   impact 1.0
   title 'safety shutdown feature and time taken to stop the server'
   desc  'safety shutdown should be enabled and time taken to stop the server should be less than or equal to 3 seconds'
-  describe ini( jetty_conf )do
-    #jetty server can be safely shutdown by pressing ctrl+c to make this enable stopAtShutdown should be true
-    its(['jetty.server.stopAtShutdown']) { should eq 'true' }
-    #Default stopTimeout of jetty is 5000 (5 seconds) its too long so stopTimeout can be changed to 3000 (3 seconds)
-    its(['jetty.server.stopTimeout']) { should eq '3000' }
+  describe ini(jetty_conf)do
+     #jetty server can be safely shutdown by pressing ctrl+c to make this enable stopAtShutdown should be true
+     its(['jetty.server.stopAtShutdown']) { should eq 'true' }
+     #Default stopTimeout of jetty is 5000 (5 seconds) its too long so stopTimeout can be changed to 3000 (3 seconds)
+     its(['jetty.server.stopTimeout']) { should eq '3000' }
   end
 end
 
@@ -215,10 +215,10 @@ control 'jetty-14'do
   impact 1.0
   title 'checking error dispatch and blocking time out'
   desc  'Error dispatch is use to prevent looping .block time out is the time take to block a IO operation'
-  describe ini( jetty_conf )do
-    #to prevent looping maximum error dispatch is changed to 5 to increse the performance
-    its(['jetty.httpConfig.maxErrorDispatches']) { should eq '5' }
-    #Maximum time to block in total for a blocking IO operation
-    its(['jetty.httpConfig.blockingTimeout']) { should eq '-1' }
+  describe ini(jetty_conf)do
+     #to prevent looping maximum error dispatch is changed to 5 to increse the performance
+     its(['jetty.httpConfig.maxErrorDispatches']) { should eq '5' }
+     #Maximum time to block in total for a blocking IO operation
+     its(['jetty.httpConfig.blockingTimeout']) { should eq '-1' }
   end
 end
